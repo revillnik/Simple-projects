ffa9467851e87317083fcb022c52a337ca8fa8b1
revillnik <revillnik@gmail.com>
2023-12-20 22:22:03 +0300

    asd

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-s = [i for i in input()]
-k = s.copy()
-for d in range(-25,0):
-    z = list("abcdefghijklmnopqrstuvwxyz")
-    for i in range(len(s)):
-        if s[i].lower() in z:
-            k[i] = z[(z.index(s[i].lower()) + d) % len(z)]
-    k[0] = k[0].upper()
-    print("".join(k))
+s = [i for i in input().split(" ")]
+z = list("abcdefghijklmnopqrstuvwxyz")
+f = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+k = []
+def bykv(str):
+    c = 0
+    for i in str:
+        if i.isalpha():
+            c += 1
+    return c
+for d in range(len(s)):
+    o = list(s[d])
+    for i in range(len(o)):
+        if o[i] in z:
+            o[i] = z[(z.index(o[i]) + bykv(s[d])) % len(z)]
+        if o[i] in f:
+            o[i] = f[(f.index(o[i]) + bykv(s[d])) % len(f)]
+    k.append("".join(o))
+print(" ".join(k))
