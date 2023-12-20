bdf8191a262785e0aecd0ce4c0a553d7d737e0da
revillnik <revillnik@gmail.com>
2023-12-20 21:08:25 +0300

    уныу

---
+++ 4Cezar.py
@@ -0,0 +1,8 @@
+z = list('абвгдежзийклмнопрстуфхцчшщъыьэюя')
+s = [i for i in input()]
+n = int(input())
+for i in range(len(s)):
+    if s[i].lower() in z:
+        s[i] = z[(z.index(s[i].lower()) + n) % len(z)]
+s[0] = s[0].upper()
+print("".join(s))
