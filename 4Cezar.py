6186056b9fadc7042d27cb0d1cf9e6f5f136f66e
revillnik <revillnik@gmail.com>
2023-12-20 22:45:43 +0300

    yese

--- 4Cezar.py
+++ 4Cezar.py
@@ -1,10 +1,41 @@
 
-z = list('абвгдежзийклмнопрстуфхцчшщъыьэюя')
-s = [i for i in input()]
-n = int(input())
-for i in range(len(s)):
-    if s[i].lower() in z:
-        s[i] = z[(z.index(s[i].lower()) + n) % len(z)]
-s[0] = s[0].upper()
-print("".join(s))
+def bykv(str):
+    c = 0
+    for i in str:
+        if i.isalpha():
+            c += 1
+    return c
+def shifr (z, f, x):
+	for d in range(len(s)):
+		o = list(s[d])
+		for i in range(len(o)):
+			if o[i] in z:
+					o[i] = z[(z.index(o[i]) + bykv(s[d])*x) % len(z)]
+			if o[i] in f:
+					o[i] = f[(f.index(o[i]) + bykv(s[d])*x) % len(f)]
+		k.append("".join(o))
+	print(" ".join(k))
+
+en = list("abcdefghijklmnopqrstuvwxyz")
+enb = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+k = []
+ru = list('абвгдежзийклмнопрстуфхцчшщъыьэюя')
+rub = list('АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')
+print('Добро пожаловать в шифровальную машину Цезаря \nЧто бы вы хотели сделать, зашифровать или дешифровать сообщение?')
+if input() in 'зашифроватьЗашифровать':
+	x = 1
+else:
+	x = -1
+print('На каком языке сообщение? Русский или английский?')
+if input() in "Русскийрусский":
+	print('Введите сообщение')
+	s = [i for i in input().split(" ")]
+	print('Ваше сообщение будет таким:')
+	shifr (ru, rub, x)
+else:
+	print('Введите сообщение')
+	s = [i for i in input().split(" ")]
+	print('Ваше сообщение будет таким:')
+	shifr(en, enb, x)
+
 
